4c11e661413553aa4478477774b99d7f6a55c76e
kanyapa12 <kanyapa.p66@rsu.ac.th>
2025-10-20 19:50:24 +0700

    ..

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ahhh ha'
+  database: process.env.DB_NAME || 'ahhhha'
 });
 
 // Create a new Express app
